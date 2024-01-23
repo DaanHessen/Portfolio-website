575a9e0d0c0070b746a0c8d0d67aed02ea7ea64d
DaanHessen <daanh2002@gmail.com>
2024-01-23 15:27:21 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+
+class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = '!<>-_\\/[]{}—=+*^?#________';
+    this.update = this.update.bind(this);
+  }
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise(resolve => this.resolve = resolve);
+    this.queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || '';
+      const to = newText[i] || '';
+      const start = Math.floor(Math.random() * 40) * 2;
+      const end = start + Math.floor(Math.random() * 40) * 2;
+      this.queue.push({ from, to, start, end });
+    }
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+  update() {
+    let output = '';
+    let complete = 0;
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+        output += `<span class="dud">${char}</span>`;
+      } else {
+        output += from;
+      }
+    }
+    this.el.innerHTML = output;
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }}
+
+
+// ——————————————————————————————————————————————————
+// Example
+// ——————————————————————————————————————————————————
+
+const phrases = [
+'Hi,',
+'I\'m Daan.',
+'I\'m still creating my website.',
+'Want to see some of my work?',
+'Check out my GitHub.',
+'Full website coming soon.'];
+
+
+const el = document.querySelector('.text');
+const fx = new TextScramble(el);
+
+let counter = 0;
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    let timeoutDuration = 1500; // default duration for all phrases except the last two
+    if (counter === phrases.length - 2) {
+      timeoutDuration = 1500; // duration for the second to last phrase
+    }
+    if (counter === phrases.length - 1) {
+      timeoutDuration = 5000; // duration for the last phrase
+    }
+    counter = (counter + 1) % phrases.length;
+    setTimeout(next, timeoutDuration);
+  });
+};
+
+next();
